94f89b681d82903c0d222a1ee5ea1ddeebf38127
jackpbauer-work <124083011+jackpbauer-work@users.noreply.github.com>
2023-04-10 13:28:34 -0400

    putting it all together

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count=0):
+        self.title = title # creating an attribute called title and assigning it the value of the title parameter
+        self.page_count = page_count # creating an attribute called page_count and assigning it the value of the page_count parameter
+
     pass
+
+    def __str__(self):
+        return self.title # returns the value of the title attribute
+
+    def get_page_count(self): # self is a reference to the instance of the class
+
+        return self._page_count # self.page_count is an attribute of the Book class
+    
+    def set_page_count(self, page_count): # creating a getter method for the page_count attribute
+
+
+        if isinstance(page_count, int): # isinstance is a built-in function that returns True if the object is an instance of the class
+
+            self._page_count = page_count # in this case, it checks if page_count is an integer. if it is, it assigns the value of page_count to the attribute _page_count
+
+        else:
+            print("page_count must be an integer") 
+            self._page_count = None # if it is not, it prints an error message and assigns None to the attribute _page_count.
+            
+        
+    page_count = property(get_page_count, set_page_count) # property is a built-in function that returns a property attribute
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!") # prints a string
+
+
+# Path: lib/book.py
+    
+
+    
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand): # the __init__ method is called when an instance of the class is created and it initializes the attributes of the class with the values passed in as parameters when the instance is created \
+        self.brand = brand
+
+
+    def get_size(self): # creating a getter method for the size attribute
+        return self._size # self.size is an attribute of the Shoe class
+    
+    def set_size(self, size):
+        if isinstance(size, int): # isinstance is a built-in function that returns True if the object is an instance of the class for example, isinstance(1, int) returns True because 1 is an integer and isinstance("1", int) returns False because "1" is not an integer
+            self._size = size
+        else:
+            print("size must be an integer") # if the size parameter is not an integer, it prints an error message and assigns None to the attribute _size 
+            self._size = None # self._size is an attribute of the Shoe class and it is private because it starts with an underscore and it is not meant to be accessed outside of the class definition 
+
+    def cobble(self): # creating a method called cobble that prints a string and assigns the value "New" to the condition attribute of the instance of the class that called the method and returns None
+        print("Your shoe is as good as new!") # prints a string 
+        self.condition = "New" # self.condition is an attribute of the Shoe class and it is public because it does not start with an underscore and it is meant to be accessed outside of the class definition
+
+    size = property(get_size, set_size) # property is a built-in function that returns a property attribute and it takes two parameters, the first is the getter method and the second is the setter method and it assigns the property attribute to the size attribute of the Shoe class and it is public because it does not start with an underscore and it is meant to be accessed outside of the class definition 
